3b2e40d700648166123fcd758be14ec64eb0abbc
amera-sinanovic1 <amera.sinanovic@fet.ba>
2024-10-09 07:20:26 +0200

    Add files via upload

---
+++ LV1/helloWorld/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  println("Hello World!") //ispis + prelaz u novi red
+  print("Hello World!")
+  println("Hello World!")
+
+}
+
