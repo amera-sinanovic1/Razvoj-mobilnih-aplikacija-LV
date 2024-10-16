259a7f9637d2bc0bd0dd28c8fef16bae1097455e
amera-sinanovic1 <amera.sinanovic@fet.ba>
2024-10-16 03:15:56 +0200

    Add files via upload

---
+++ LV2/LV2_primjer1/src/Main.kt
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //U gradu Cifrogradu, postojala je lista brojeva nazvana nums koja je sadržavala
+// cijele brojeve od 0 do n - 1. Svaki broj je trebao da se pojavi tačno jednom na listi,
+// međutim, dva nestašna broja su se pojavila još jednom, čineći listu dužom nego obično.
+    //
+    //Kao detektiv grada, vaš zadatak je da pronađete ova dva nestašna broja.
+//  Kao reyultat funkcije Vratite niz duzine dva koji sadrži ova dva broja nestasna
+//  (u bilo kom redoslijedu), kako bi mir mogao da se vrati u Cifrograd.
+    //Input: nums = [0,1,1,0]
+    //
+    //Output: [0,1]
+    //
+    //Input: nums = [0,3,2,1,3,2]
+    //
+    //Output: [2,3]
+    //
+    //Explanation:
+
+}
+
+fun getSneakyNumbers(nums: IntArray): IntArray {
+
+}
